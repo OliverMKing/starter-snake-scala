d81b1265460d682c10d6ea9338d7a8fe38c0383a
Oliver King <33605867+OliverMKing@users.noreply.github.com>
2020-08-01 13:03:09 -0400

    Add Finch http server

--- build.sbt
+++ build.sbt
@@ -3,3 +3,8 @@ name := "starter-snake-scala"
 version := "0.1"
 
 scalaVersion := "2.12.6"
+
+libraryDependencies ++= Seq(
+  "com.github.finagle" %% "finchx-core" % "0.32.0",
+  "com.github.finagle" %% "finchx-circe" % "0.32.0",
+)
--- src/main/scala/io/battlesnake/Main.scala
+++ src/main/scala/io/battlesnake/Main.scala
@@ -1,5 +1,22 @@
 package io.battlesnake
 
-object Main extends App {
-  println("Hello World!")
+import io.finch._
+import cats.effect.IO
+import com.twitter.finagle.{Http, Service}
+import com.twitter.finagle.http.{Request, Response}
+import com.twitter.util.Await
+
+object Main extends App with Endpoint.Module[IO] {
+  println("Starting server...")
+
+  def index: Endpoint[IO, String] = get(pathEmpty) { Ok("Hello World!")}
+
+
+  def service: Service[Request, Response] = Bootstrap
+    .serve[Text.Plain](index)
+    .toService
+
+
+  // Start server
+  Await.ready(Http.server.serve(":8080", service))
 }
